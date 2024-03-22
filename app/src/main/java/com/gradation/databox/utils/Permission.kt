39abd7d850bb63224e5719188cc0dc277f447074
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-03-22 11:33:38 +0900

    feat: feat permission - 권한관련 내용을 manifest에 추가합니다. - 버전에 따른 permission 정보를 추가합니다.

---
+++ app/src/main/java/com/gradation/databox/utils/Permission.kt
@@ -0,0 +1,22 @@
+package com.gradation.databox.utils
+
+import android.Manifest
+import android.os.Build
+
+
+val permissionList: List<String> =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        listOf(
+            Manifest.permission.READ_MEDIA_IMAGES,
+            Manifest.permission.READ_MEDIA_VIDEO,
+            Manifest.permission.READ_MEDIA_AUDIO
+        )
+
+    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S_V2) {
+        listOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+    } else
+        listOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
